ece0c823e199c9a59aa97441bdd8ffec087d2e4c
Amaan-Tamboli <89582825+Amaan-Tamboli@users.noreply.github.com>
2022-11-09 01:40:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,14 @@ from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 
 
-with open("styles.css") as f:
-    st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
+
     
 add_selectbox = st.sidebar.selectbox("Details/Developer",("Prediction","Developer"))
 if add_selectbox== "Developer":
     developer()
 elif(add_selectbox=="Prediction"):
     model = joblib.load('Real-Fake')
-    st.markdown('<h1 style="text-align:center;color:white;font-weight:bolder;font-size:100px;">FAKE NEWS PREDICTION</h1>',unsafe_allow_html=True)
+    st.markdown('<h1 style="text-align:center;color:black;font-weight:bolder;font-size:100px;">FAKE NEWS PREDICTION</h1>',unsafe_allow_html=True)
 
     ip = st.text_input('Enter the news article : ')
     op = model.predict([ip])
