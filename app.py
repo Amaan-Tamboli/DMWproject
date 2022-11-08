9c153c1ce22a9ed223e6a8b9a320fa4d6275638d
Amaan-Tamboli <89582825+Amaan-Tamboli@users.noreply.github.com>
2022-11-09 01:01:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,23 +10,21 @@ vect = TfidfVectorizer(stop_words='english')
 add_selectbox = st.sidebar.selectbox("Details/Developer",("Prediction","Developer"))
 if add_selectbox== "Developer":
     developer()
-    
-model = joblib.load('Real-Fake')
-st.markdown('<h1 style="text-align:center;color:black;font-weight:bolder;font-size:100px;">FAKE NEWS PREDICTION</h1>',unsafe_allow_html=True)
+elif(add_selectbox=="Prediction"):
+    model = joblib.load('Real-Fake')
+    st.markdown('<h1 style="text-align:center;color:black;font-weight:bolder;font-size:100px;">FAKE NEWS PREDICTION</h1>',unsafe_allow_html=True)
 
-ip = st.text_input('Enter the news article : ')
-op = model.predict([ip])
-if st.button('Predict'):
-  st.title(op[0])
-  
- 
-wordcloud = WordCloud(width = 800, height = 800,
-                background_color ='black',
-                stopwords = STOPWORDS,
-                min_font_size = 10).generate(ip)
-                      
-plt.figure(figsize = (8, 8), facecolor = None)
-plt.imshow(wordcloud)
-plt.axis("off")
-plt.tight_layout(pad = 0)
-st.pyplot(fig)
+    ip = st.text_input('Enter the news article : ')
+    op = model.predict([ip])
+    if st.button('Predict'):
+      st.title(op[0])
+
+
+    wordcloud = WordCloud(max_words = 2000 , width = 1600 , height = 800 , stopwords = STOPWORDS).generate(m)
+    
+
+    fig = plt.figure(figsize = (8, 8), facecolor = None)
+    plt.imshow(wordcloud , interpolation = 'bilinear')
+    plt.axis("off")
+    plt.tight_layout(pad = 0)
+    st.pyplot(fig)
