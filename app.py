c5d6feedc055e445c8dc8c5c350ae1d107a3d88b
Amaan-Tamboli <89582825+Amaan-Tamboli@users.noreply.github.com>
2022-11-09 01:34:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,17 @@ import streamlit as st
 from developer import developer
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
-from sklearn.feature_extraction.text import TfidfVectorizer
-vect = TfidfVectorizer(stop_words='english')
 
 
+with open("styles/style.css") as f:
+    st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
+    
 add_selectbox = st.sidebar.selectbox("Details/Developer",("Prediction","Developer"))
 if add_selectbox== "Developer":
     developer()
 elif(add_selectbox=="Prediction"):
     model = joblib.load('Real-Fake')
-    st.markdown('<h1 style="text-align:center;color:black;font-weight:bolder;font-size:100px;">FAKE NEWS PREDICTION</h1>',unsafe_allow_html=True)
+    st.markdown('<h1 style="text-align:center;color:white;font-weight:bolder;font-size:100px;">FAKE NEWS PREDICTION</h1>',unsafe_allow_html=True)
 
     ip = st.text_input('Enter the news article : ')
     op = model.predict([ip])
