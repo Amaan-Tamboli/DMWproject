67ff93c1b9081d450a6a85240adf2f51285104e7
Amaan-Tamboli <89582825+Amaan-Tamboli@users.noreply.github.com>
2022-11-08 23:42:10 +0530

    Create developer.py

---
+++ developer.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import streamlit as st
+
+
+def developer():
+    st.title("Developers")
+    st.write(""" Linkedin : https://www.linkedin.com/in/amaan-tamboli/""")
+
+    st.write("""Linkedin : https://www.linkedin.com/in/mahesh-yarra-997963214/""")
+    
+    st.write("""Linkedin : https://www.linkedin.com/in/kunal-yeole-2b005b208/""")
+    
+    st.write("""Linkedin : https://www.linkedin.com/in/siddhanttayade/""")
