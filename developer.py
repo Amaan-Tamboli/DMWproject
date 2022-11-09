1594512ebdbda466ebe8e623a39dd0e30d707552
Mahesh-Yarra <91123956+Mahesh-Yarra@users.noreply.github.com>
2022-11-09 11:51:28 +0530

    Update developer.py

--- developer.py
+++ developer.py
@@ -4,10 +4,10 @@ import streamlit as st
 
 def developer():
     st.title("Developers")
-    st.write(""" Amaan Tamboli[link]https://www.linkedin.com/in/amaan-tamboli/""")
+    st.write(""" Amaan Tamboli[link](https://www.linkedin.com/in/amaan-tamboli/)""")
 
-    st.write("""Mahesh Yarra : https://www.linkedin.com/in/mahesh-yarra-997963214/""")
+    st.write("""Mahesh Yarra[link](https://www.linkedin.com/in/mahesh-yarra-997963214/)""")
     
-    st.write("""Kunal Yeole : https://www.linkedin.com/in/kunal-yeole-2b005b208/""")
+    st.write("""Kunal Yeole[link](https://www.linkedin.com/in/kunal-yeole-2b005b208/)""")
     
-    st.write("""Siddhant Tayade : https://www.linkedin.com/in/siddhanttayade/""")
+    st.write("""Siddhant Tayade[link](https://www.linkedin.com/in/siddhanttayade/)""")
